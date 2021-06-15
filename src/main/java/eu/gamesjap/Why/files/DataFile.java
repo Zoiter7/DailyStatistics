b7f36ea10cf91a70490a18f2783ee99cf626312a
OopsieWoopsie <xiaosheidy@gmail.com>
2021-06-15 19:15:31 -0300

    Applied eclipse auto-format

--- src/main/java/eu/gamesjap/Why/files/DataFile.java
+++ src/main/java/eu/gamesjap/Why/files/DataFile.java
@@ -10,48 +10,48 @@ import net.md_5.bungee.config.YamlConfiguration;
 
 public class DataFile {
 
-	private Configuration dataConfig;
-	private static File dataFile;
+    private Configuration dataConfig;
+    private static File dataFile;
 
-	public void createFile() {
+    public void createFile() {
 
-		File folder = DailyStatistics.plugin.getDataFolder();
-		if(!folder.exists()) {
-			folder.mkdir();
-		}
+        File folder = DailyStatistics.plugin.getDataFolder();
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
 
-		dataFile = new File(folder + File.separator +  "data" + ".yml");
+        dataFile = new File(folder + File.separator + "data" + ".yml");
 
-		if(!dataFile.exists()) {
-			try {
-				dataFile.createNewFile();
-			}catch(IOException e) {
-				DailyStatistics.plugin.getProxy().getConsole().sendMessage("§CAn error has occurred when creating file...");
+        if (!dataFile.exists()) {
+            try {
+                dataFile.createNewFile();
+            } catch (IOException e) {
+                DailyStatistics.plugin.getProxy().getConsole()
+                        .sendMessage("§CAn error has occurred when creating file...");
 
-			}
-		}  
+            }
+        }
 
-		try {
-			dataConfig = ConfigurationProvider.getProvider(YamlConfiguration.class).load(dataFile);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            dataConfig = ConfigurationProvider.getProvider(YamlConfiguration.class).load(dataFile);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void saveData() {
-		try {
-			ConfigurationProvider.getProvider(YamlConfiguration.class).save(dataConfig, dataFile);
+    public void saveData() {
+        try {
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(dataConfig, dataFile);
 
-		}catch(IOException e) {
+        } catch (IOException e) {
 
-		}
-	}
+        }
+    }
 
-	public Configuration getData() {
-		return dataConfig;
-	}
+    public Configuration getData() {
+        return dataConfig;
+    }
 
 }
