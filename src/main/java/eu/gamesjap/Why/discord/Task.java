d8a91eb6e57babd984e2086362d5a900a44a494e
Jonas T. Schmid <jonas.till.schmid@gmail.com>
2021-09-25 09:15:41 +0200

    Fixed that the daily stats only show up once.

--- src/main/java/eu/gamesjap/Why/discord/Task.java
+++ src/main/java/eu/gamesjap/Why/discord/Task.java
@@ -1,6 +1,7 @@
 package eu.gamesjap.Why.discord;
 
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 import eu.gamesjap.Why.DailyStatistics;
 
@@ -13,7 +14,9 @@ public class Task extends TimerTask {
     }
 
     public void run() {
-        ds.prepareDiscordMessage(ds.getActualDate(), false, null);
+        // ds.prepareDiscordMessage(ds.getActualDate(), false, null);
         ds.getLogger().info(ds.prefix + "§aDay finished! (maybe?) Running discord task... (Sending message to your discord server)");
+        ds.getProxy().getScheduler().schedule(ds, () -> ds.prepareDiscordMessage(ds.getActualDate(), false, null), 0, 1, TimeUnit.DAYS);
+
     }
 }
